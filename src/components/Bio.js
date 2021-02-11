8abd394905beaba421fe6a683f1de4e6bdae2e4c
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2021-02-11 14:46:12 -0700

    Changed background color and added poster image on bio page

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,21 +2,26 @@ import React from 'react';
 import '../App.css';
 import techgawd from '../images/techgawd.jpg';
 import avatar from '../images/avatar.png'
+import aboutMe from "../images/aboutMe.png";
 import Nav from './Nav';
 
 function Bio() {
   return (
     <div className="App">
-    <Nav />
-    <section className="bio">
-    <h1 className="debug">.</h1>
-    <section className="bioSection">
-    <img alt="" className='mePic' src={techgawd} />
-    <p>I am Angela and I will write lots of stuff here</p>
-    <img className='avatarPic' alt=""  src={avatar} />
-    </section>
+      <Nav />
+      <section className="bio">
+        <h1 className="debug">.</h1>
+        <section className="bioSection">
+          {/* <img alt="" className='mePic' src={techgawd} /> */}
+          {/* <p className="bioText">Okay maybe I'm not a mother of actual dragons. I am a fan of the first seven seasons of Game of Thrones and I'm a mother of three human sons if that counts! </p> */}
+          <img className='aboutMe' alt="" src={aboutMe} />
+          {/* <img className='avatarPic' alt="" src={avatar} /> */}
+        </section>
 
-    </section>
+      </section>
+      <footer className="bioFooter">
+        @Liteworker 2021
+    </footer>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Projects from './Projects';
 import LiteworkerLogo_Horiz_Transparent from '../images/LiteworkerLogo_Horiz_Transparent.png';
 import headshot from '../images/headshot.JPG';
+import border from '../images/border.png';
 import avatar from '../images/avatar.png'
 import { Route, Link } from 'react-router-dom';
 // import Slider from './Slider';
@@ -13,46 +14,47 @@ import { Route, Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="App">
-    <img className='logo' alt=""  src={LiteworkerLogo_Horiz_Transparent} />
-    <br />
-    <Route exact path="/portfolio" component={Projects} />
-    <Button variant="warning"><Link className="link" to="/">HOME</Link></Button>
-    <Button variant="warning"><Link className="link" to="/portfolio">PORTFOLIO</Link></Button>
-    <Button variant="warning"><Link className="link" to="/bio">BIO</Link></Button>
-    <Button variant="warning"><Link className="link" to="/contact">CONTACT</Link></Button>
-    <section className="home">
-    <div className="heroSection">
-    <h2 className='hero'>Hi, I'm Angela Williams<br /><br /><br />Frontend Developer, Medium writer, Tarot reader, yoga teacher and mother of dragons ;)</h2>
-    <img className='headshot' alt=""  src={headshot} />
+      <img className='logo' alt="" src={LiteworkerLogo_Horiz_Transparent} />
+      <br />
+      <Route exact path="/portfolio" component={Projects} />
+      <Button variant="warning"><Link className="link" to="/">HOME</Link></Button>
+      <Button variant="warning"><Link className="link" to="/portfolio">PORTFOLIO</Link></Button>
+      <Button variant="warning"><Link className="link" to="/bio">BIO</Link></Button>
+      <Button variant="warning"><Link className="link" to="/contact">CONTACT</Link></Button>
+      <section className="home">
+        {/* <img className='border' alt="" src={border} /> */}
+        <div className="heroSection">
+          <h2 className='hero'>Hi, I'm Angela Williams<br /><br /><br />Frontend Developer, Medium writer, Tarot reader, yoga teacher and mother of dragons ;)</h2>
+          <img className='headshot' alt="" src={headshot} />
 
-    <div className="avatarSection">
-    <div className='avatar1'>
-    {/* <h1 class="bounce">An animated element</h1> */}
-    <ul>
+          <div className="avatarSection">
+            <div className='avatar1'>
+              {/* <h1 class="bounce">An animated element</h1> */}
+              {/* <ul>
       <li class="bounce">React</li>
       <li class="bounce">CSS</li>
       <li class="bounce">HTML</li>
       <li class="bounce">Javascript</li>
-      </ul>
-    </div>
-    <div className='avatar2'>
-    <img className='avatarPic' alt=""  src={avatar} />
-    </div>
-    <div className='avatar3'>
-    {/* <h1 class="bounce">An animated element</h1> */}
-    <ul>
-      <li class="bounce">React</li>
-      <li class="bounce">CSS</li>
-      <li class="bounce">HTML</li>
-      <li class="bounce">Javascript</li>
-      </ul>
-    </div>
-    </div>
-    </div>
-    <footer>
-      @Liteworker 2021
+      </ul> */}
+            </div>
+            <div className='avatar2'>
+              <img className='avatarPic' alt="" src={avatar} />
+            </div>
+            <div className='avatar3'>
+              {/* <h1 class="bounce">An animated element</h1> */}
+              {/* <ul>
+                <li class="bounce">React</li>
+                <li class="bounce">CSS</li>
+                <li class="bounce">HTML</li>
+                <li class="bounce">Javascript</li>
+              </ul> */}
+            </div>
+          </div>
+        </div>
+        <footer>
+          @Liteworker 2021
     </footer>
-    </section>
+      </section>
     </div>
   );
 }
